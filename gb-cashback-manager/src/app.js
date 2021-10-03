4cbb2cfd4f33e61dc362c6406e1aa06cba623b26
Evandro Junior <evandro.arede@compasso.com.br>
2021-10-03 03:25:04 -0300

    add initial main scripts

---
+++ gb-cashback-manager/src/app.js
@@ -0,0 +1,30 @@
+import express from 'express';
+
+import loggerMid, { logger } from './middlewares/logger.js';
+import timeoutMid from './middlewares/timeout.js';
+// import securityMid from './middlewares/security.js';
+import routes from './routes/index.js';
+import errorHandler from './middlewares/errorHandler.js';
+
+/**
+ * Init the main application
+ * @param {boolean} showLOG
+ * @param {number} serverTimeout
+ */
+
+const initApp = ({ showLOG = false, serverTimeout = 30000 } = {}) => {
+    const app = express();
+    app.disable('x-powered-by');
+    app.use(express.json());
+    if (showLOG) app.use(loggerMid);
+    app.use(timeoutMid(serverTimeout));
+    // app.use(securityMid);
+    app.use('/v1', routes);
+    app.use(...errorHandler);
+    return app;
+};
+
+export {
+    initApp as default,
+    logger,
+};
