c7e12452706659d9ea6401cc48d75cc170056c82
Evandro Junior <evandro.arede@compasso.com.br>
2021-10-06 00:04:57 -0300

    update database definition

--- database/GB-CASHBACK.sql
+++ database/GB-CASHBACK.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: mysql
--- Tempo de geração: 04-Out-2021 às 17:14
+-- Tempo de geração: 06-Out-2021 às 03:03
 -- Versão do servidor: 8.0.26
 -- versão do PHP: 7.4.20
 
@@ -31,16 +31,19 @@ USE `GB-CASHBACK`;
 
 DROP TABLE IF EXISTS `AUTO_STATUS_PURCHASE_EVENTS`;
 CREATE TABLE `AUTO_STATUS_PURCHASE_EVENTS` (
+  `idAutoStatus` bigint UNSIGNED NOT NULL,
   `cpf` varchar(11) NOT NULL COMMENT 'CPF of a retailer, registered or not',
-  `idStatus` bigint UNSIGNED NOT NULL COMMENT 'Purchase status identifier'
+  `idStatus` bigint UNSIGNED NOT NULL COMMENT 'Purchase status identifier',
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Extraindo dados da tabela `AUTO_STATUS_PURCHASE_EVENTS`
 --
 
-INSERT INTO `AUTO_STATUS_PURCHASE_EVENTS` (`cpf`, `idStatus`) VALUES
-('15350946056', 2);
+INSERT INTO `AUTO_STATUS_PURCHASE_EVENTS` (`idAutoStatus`, `cpf`, `idStatus`, `createdAt`, `updatedAt`) VALUES
+(1, '15350946056', 2, '2021-10-04 19:19:32', '2021-10-04 19:19:32');
 
 -- --------------------------------------------------------
 
@@ -53,9 +56,10 @@ CREATE TABLE `PURCHASE` (
   `idPurchase` bigint UNSIGNED NOT NULL COMMENT 'Purchase identifier',
   `code` varchar(20) NOT NULL COMMENT 'Purchase unique code',
   `amount` float NOT NULL COMMENT 'Purchase value',
-  `date` datetime NOT NULL COMMENT 'Purchase date',
+  `date` date NOT NULL COMMENT 'Purchase date',
   `idRetailer` bigint UNSIGNED NOT NULL COMMENT 'Retailer identifier that does this purchase',
-  `registerDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Purchase registration date'
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -71,8 +75,9 @@ CREATE TABLE `RETAILER` (
   `cpf` varchar(11) NOT NULL COMMENT 'Retailer CPF document',
   `email` text NOT NULL COMMENT 'Retailer email',
   `hashedPassword` text NOT NULL COMMENT 'Retailer hash password',
-  `registerDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT 'Retailer registration date',
-  `previousCashbackBalance` float NOT NULL COMMENT 'Retailer previous cashback balance'
+  `previousCashbackBalance` float NOT NULL COMMENT 'Retailer previous cashback balance',
+  `createdAt` datetime NOT NULL,
+  `updatedAt` datetime NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 -- --------------------------------------------------------
@@ -84,17 +89,19 @@ CREATE TABLE `RETAILER` (
 DROP TABLE IF EXISTS `STATUS_PURCHASE`;
 CREATE TABLE `STATUS_PURCHASE` (
   `idStatus` bigint UNSIGNED NOT NULL COMMENT 'Purchase registration status identifier',
-  `statusName` text NOT NULL COMMENT 'Status name'
+  `statusName` text NOT NULL COMMENT 'Status name',
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
 -- Extraindo dados da tabela `STATUS_PURCHASE`
 --
 
-INSERT INTO `STATUS_PURCHASE` (`idStatus`, `statusName`) VALUES
-(1, 'Em validação'),
-(2, 'Aprovado'),
-(3, 'Negado');
+INSERT INTO `STATUS_PURCHASE` (`idStatus`, `statusName`, `createdAt`, `updatedAt`) VALUES
+(1, 'Em validação', '2021-10-04 20:22:28', '2021-10-04 20:22:28'),
+(2, 'Aprovado', '2021-10-04 20:22:28', '2021-10-04 20:22:28'),
+(3, 'Negado', '2021-10-04 20:22:28', '2021-10-04 20:22:28');
 
 -- --------------------------------------------------------
 
@@ -107,7 +114,8 @@ CREATE TABLE `STATUS_PURCHASE_EVENTS` (
   `idStatusEvent` bigint UNSIGNED NOT NULL COMMENT 'Status event identifier',
   `idPurchase` bigint UNSIGNED NOT NULL COMMENT 'Purchase identifier',
   `idStatus` bigint UNSIGNED NOT NULL COMMENT 'Purchase register status identifier',
-  `statusDate` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'Date when status has been attributed'
+  `createdAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updatedAt` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 --
@@ -118,6 +126,7 @@ CREATE TABLE `STATUS_PURCHASE_EVENTS` (
 -- Índices para tabela `AUTO_STATUS_PURCHASE_EVENTS`
 --
 ALTER TABLE `AUTO_STATUS_PURCHASE_EVENTS`
+  ADD PRIMARY KEY (`idAutoStatus`),
   ADD KEY `idStatus` (`idStatus`);
 
 --
@@ -125,10 +134,7 @@ ALTER TABLE `AUTO_STATUS_PURCHASE_EVENTS`
 --
 ALTER TABLE `PURCHASE`
   ADD PRIMARY KEY (`idPurchase`),
-  ADD UNIQUE KEY `idPurchase` (`idPurchase`),
-  ADD UNIQUE KEY `CODE` (`code`),
   ADD UNIQUE KEY `PURCHASE_CODE` (`code`),
-  ADD KEY `ID_RETAILER` (`idRetailer`),
   ADD KEY `PURCHASE_ID_RETAILER` (`idRetailer`);
 
 --
@@ -136,7 +142,6 @@ ALTER TABLE `PURCHASE`
 --
 ALTER TABLE `RETAILER`
   ADD PRIMARY KEY (`idRetailer`),
-  ADD UNIQUE KEY `idRetailer` (`idRetailer`),
   ADD UNIQUE KEY `CPF` (`cpf`),
   ADD UNIQUE KEY `EMAIL` (`email`(100));
 
@@ -144,15 +149,13 @@ ALTER TABLE `RETAILER`
 -- Índices para tabela `STATUS_PURCHASE`
 --
 ALTER TABLE `STATUS_PURCHASE`
-  ADD PRIMARY KEY (`idStatus`),
-  ADD UNIQUE KEY `idStatus` (`idStatus`);
+  ADD PRIMARY KEY (`idStatus`);
 
 --
 -- Índices para tabela `STATUS_PURCHASE_EVENTS`
 --
 ALTER TABLE `STATUS_PURCHASE_EVENTS`
   ADD PRIMARY KEY (`idStatusEvent`),
-  ADD UNIQUE KEY `idStatusEvent` (`idStatusEvent`),
   ADD KEY `STATUS_EVENT_ID_PURCHASE` (`idPurchase`),
   ADD KEY `idStatus` (`idStatus`);
 
@@ -161,16 +164,22 @@ ALTER TABLE `STATUS_PURCHASE_EVENTS`
 --
 
 --
+-- AUTO_INCREMENT de tabela `AUTO_STATUS_PURCHASE_EVENTS`
+--
+ALTER TABLE `AUTO_STATUS_PURCHASE_EVENTS`
+  MODIFY `idAutoStatus` bigint UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT de tabela `PURCHASE`
 --
 ALTER TABLE `PURCHASE`
-  MODIFY `idPurchase` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Purchase identifier';
+  MODIFY `idPurchase` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Purchase identifier', AUTO_INCREMENT=10;
 
 --
 -- AUTO_INCREMENT de tabela `RETAILER`
 --
 ALTER TABLE `RETAILER`
-  MODIFY `idRetailer` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Retailer primary key';
+  MODIFY `idRetailer` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Retailer primary key', AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT de tabela `STATUS_PURCHASE`
@@ -182,7 +191,7 @@ ALTER TABLE `STATUS_PURCHASE`
 -- AUTO_INCREMENT de tabela `STATUS_PURCHASE_EVENTS`
 --
 ALTER TABLE `STATUS_PURCHASE_EVENTS`
-  MODIFY `idStatusEvent` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Status event identifier';
+  MODIFY `idStatusEvent` bigint UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'Status event identifier', AUTO_INCREMENT=10;
 
 --
 -- Restrições para despejos de tabelas
